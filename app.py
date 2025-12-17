5b67a0544359d0e32c3eb120ba748e5f5f31c65c
Tamal Kar <bwubts23429@brainwareuniversity.ac.in>
2025-12-17 19:16:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,request,render_template
+import pandas 
+import numpy as np
+import pickle
+
+
+model=pickle.load(open("model.pkl",'rb'))
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+     return render_template("index.html")
+
+
+@app.route("/predict",methods=['POST'])
+def predic():
+     featurs=request.form['feature']
+     featurs_lst=featurs.split(',')
+     np_featurs=np.asarray(featurs_lst,dtype=np.float32)
+     pred=model.predict(np_featurs.reshape(1,-1))
+
+     output = ["Cancrous" if pred[0]==1 else 'not Cancrous']
+     
+     return render_template('index.html',message=output)
+
+if __name__=="__main__":
+    app.run(debug=True)
